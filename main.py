933273dc482036339ee4318a5d650347ff2f198a
nabilwe <148687352+nabilwe@users.noreply.github.com>
2025-03-08 07:21:08 +0000

    belajar varible

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 print("hello nabil")
 print("hi,how are you?",end="")
 
+#valid indentifiers
+Myfile - "python"
+Date9_90 - "submit date"
+_ds + "hello"
+
+print(Myfile)
+print(Date9_90)
+print(_ds)
+
+"""
+#Invalid indentifier
+data rec = "pyhton"
+print(data-rec)
+"""
+
+1 x =10
+print(x)
+print(type(x))
+
+y=3.12
+print(y)
+print(type(y))
