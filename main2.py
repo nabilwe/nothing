d6ea02e55f56a1a8c2514f0057a5c5e72c985822
nabilwe <nabilmiomu@gmail.com>
2025-03-22 14:20:50 +0700

    im

--- main2.py
+++ main2.py
@@ -1,1 +1,14 @@
-print("test")
+nilai = int(input("masukkan nilai:"))
+
+if nilai >= 50:
+    print("selamat anda lulus")
+    if nilai ==100:
+        print("nilai anda sempurna")
+     elif nilai >= 9:
+        print("anda dapat nilai a")
+     else:
+        print("anda dapat nilai b")
+   else:
+    print("maaf anda tidak lulus")   
+
+    print("termakasih sudah menggunakan progam ini")        
